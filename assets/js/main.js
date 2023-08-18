1ea2b3f77d6e53d3b4677ec5de0e6318f3cc9151
Samuel Castiblanco <138372579+Akaparca@users.noreply.github.com>
2023-08-18 06:39:22 -0500

    ecomerce v1.1

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,12 +4,18 @@ import products from "./components/products.js";
 import showCart from "./components/showCart.js";
 import showMenu from "./components/showMenu.js";
 import getProducts from "./helpers/getProducts.js";
-
+import scrollHero from "./components/hero.js";
+import scrollHeader from "./components/scroll.js";
 /* UI Elements */
 
 // hidden loader 
 loader()
 
+//hero
+scrollHero()
+
+// scroll header 
+scrollHeader()
 // show menu 
 showMenu()
 
